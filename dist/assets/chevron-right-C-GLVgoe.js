02de79e9b55f8da832d2dc61d15bee75624fd276
Apex <info@apex.ps>
2026-06-30 08:43:24 +0300

    build: production build for deploy

--- dist/assets/chevron-right-C-GLVgoe.js
+++
@@ -1,6 +0,0 @@
-import{a6 as t}from"./index-DeV5DQy5.js";/**
- * @license lucide-react v0.378.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
